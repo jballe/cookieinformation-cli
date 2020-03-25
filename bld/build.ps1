bd576452c07cf22285ff5fa931dfcf634b177c6a
Jesper Balle <Jesper.Balle@valtech.com>
2020-03-25 17:07:10 +0100

    Update build tags

--- bld/build.ps1
+++ bld/build.ps1
@@ -1,22 +1,29 @@
 param(
-    $Version="1.0",
+    $Version="0.4",
     $Registry="",
-    $Name="cookieinformation-cli",
+    $Name="jballe/cookieinformation-cli",
     [Switch]$Push
 )
 
 $ErrorActionPreference = "STOP"
 
 $srcDir = Join-Path $PSScriptRoot ".." -Resolve
-(get-content (join-path $PSScriptRoot "build.json" -Resolve) | ConvertFrom-Json) | ForEach-Object {
+$buildDefPath = Join-Path $PSScriptRoot "build.json" -Resolve
+$buildDef = Get-Content $buildDefPath | ConvertFrom-Json
+Write-Host ("Found {0} build definitions" -f $buildDef.Length)
+$buildDef | ForEach-Object {
     $buildDef = $_
     $tag = $buildDef.Tag.Replace("{VERSION}", $Version)
     $fullTag = "${Registry}${Name}:${Tag}"
     $base = $_.BaseImage
     Write-Host "Building $fullTag from base $base"
+    & docker pull $base
+    $LASTEXITCODE -ne 0 | Where-Object { $_ } | ForEach-Object { throw ("Failed, exitcode was {0}" -f $LASTEXITCODE) }
     & docker build -t $fullTag --build-arg BASE_IMAGE=$base $srcDir
+    $LASTEXITCODE -ne 0 | Where-Object { $_ } | ForEach-Object { throw ("Failed, exitcode was {0}" -f $LASTEXITCODE) }
 
     if($Push) {
         & docker push $fullTag
+        $LASTEXITCODE -ne 0 | Where-Object { $_ } | ForEach-Object { throw ("Failed, exitcode was {0}" -f $LASTEXITCODE) }
     }
 }
